de39faa352f1ebd244bf2dc820dbc7fa4a320b8f
Pradosh <chandpradosh7@gmail.com>
2023-04-21 13:36:39 +0530

    Completed Navbar and working on login page

--- src/scenes/homePage/index.jsx
+++ src/scenes/homePage/index.jsx
@@ -1,5 +1,11 @@
+import { Box } from "@mui/material";
+import Navbar from "scenes/navBar";
+
 const HomePage=()=>{
-    return (<div>LoginPage</div>)
+    return (
+    <Box>
+        <Navbar/>
+    </Box>)
 }
 
 export default HomePage;
